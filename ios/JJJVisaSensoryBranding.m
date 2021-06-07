93bcd586843d54ecd66038611a8ab29e3afe80cb
james <james@antit.io>
2021-06-07 06:53:49 -0600

    1.0.2

--- ios/JJJVisaSensoryBranding.m
+++ ios/JJJVisaSensoryBranding.m
@@ -1,58 +1,45 @@
 #import "JJJVisaSensoryBranding.h"
 
 @implementation JJJVisaSensoryBranding
- 
-
 
 RCT_EXPORT_MODULE()
 
 RCT_REMAP_METHOD(show,
+                 options: (NSDictionary *)options
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
 {
     dispatch_async(dispatch_get_main_queue(), ^{
+        BOOL soundEnabled = options[@"soundEnabled"] && [options[@"soundEnabled"] boolValue];
+        BOOL constrainedFlags = options[@"constrainedFlags"] && [options[@"constrainedFlags"] boolValue];
+        BOOL checkMarkShown = options[@"checkMarkShown"] && [options[@"checkMarkShown"] boolValue];
+        BOOL hapticFeedbackEnabled = options[@"hapticFeedbackEnabled"] && [options[@"hapticFeedbackEnabled"] boolValue];
         
-        SensoryBrandingView * sensoryView;
-        sensoryView = [[SensoryBrandingView alloc] initWithFrame:CGRectMake(100,100, 500, 400)];
-        sensoryView.isSoundEnabled = true;
-        //sensoryView.backdropColor = [UIColor grayColor];
-        sensoryView.hasConstrainedFlags = true;
-        sensoryView.isCheckmarkShown = true;
-        sensoryView.isHapticFeedbackEnabled = true;
-        sensoryView.autoresizesSubviews = true;
-        sensoryView.contentMode = 0;
-        // sensoryView.backgroundColor = [UIColor redColor];
+        int red = 0;
+        int green = 0;
+        int blue = 0;
+        sscanf([options[@"backdropColor"] UTF8String], "#%02X%02X%02X", &red, &green, &blue);
+        UIColor *backdropColor = [UIColor colorWithRed:red/255.0 green:green/255.0 blue:blue/255.0 alpha:1];
         
-        UIView *container = [UIView new];
-        container.backgroundColor = [UIColor blackColor];
-        container.autoresizesSubviews = true;
-        [container addSubview:sensoryView];
+        SensoryBrandingView *sensoryView = [[SensoryBrandingView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)];
+        sensoryView.center =  CGPointMake([UIScreen mainScreen].bounds.size.width /2, [UIScreen mainScreen].bounds.size.height / 2);
         
-        UIViewController *sensoryViewController = [UIViewController new];
-        sensoryViewController.view = container;
-        sensoryViewController.title = @"as";
-        sensoryViewController.automaticallyAdjustsScrollViewInsets = true;
-        sensoryViewController.modalPresentationStyle = UIModalPresentationOverFullScreen;
-  
-       // UIViewController *rootViewController = UIApplication.sharedApplication.keyWindow.rootViewController;
-      //  [rootViewController presentViewController:sensoryViewController animated:YES completion:nil];
-       // [sensoryView animate];
-        
-        UIViewController *rootViewController = UIApplication.sharedApplication.keyWindow.rootViewController;
-        if ([rootViewController isKindOfClass:[UINavigationController class]]) {
-            [((UINavigationController *)rootViewController) pushViewController:sensoryViewController animated:YES];
-        } else {
-            UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:sensoryViewController];
-            [rootViewController presentViewController:navigationController animated:YES completion:nil];
-        }
-        
+        sensoryView.isSoundEnabled = soundEnabled;
+        sensoryView.backdropColor = backdropColor;
+        sensoryView.hasConstrainedFlags = constrainedFlags;
+        sensoryView.isCheckmarkShown = checkMarkShown;
+        sensoryView.isHapticFeedbackEnabled = hapticFeedbackEnabled;
+        sensoryView.backgroundColor = [UIColor greenColor];
+
+        UIView *overlayView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)];
+        overlayView.backgroundColor = backdropColor;
+        [overlayView addSubview:sensoryView];
+    
+       [[UIApplication sharedApplication].keyWindow addSubview:overlayView];
+
        [sensoryView animateWithCompletionHandler:^(NSError * _Nullable error) {
            NSNumber *result = @(1);
-           if ([rootViewController isKindOfClass:[UINavigationController class]]) {
-               [((UINavigationController *)rootViewController) popViewControllerAnimated:YES];
-           } else {
-               [rootViewController dismissViewControllerAnimated:YES completion:nil];
-           }
+           [overlayView removeFromSuperview];
            resolve(result);
        }];
         
